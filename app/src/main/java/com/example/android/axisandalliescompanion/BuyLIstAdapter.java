46c6c30c3762c7300b78c704694e67e5e698d21b
deadlycrawler <deadlycrawler2@gmail.com>
2017-09-16 21:53:15 -0600

    buttons on the buyPage are ready for functions to be added

--- app/src/main/java/com/example/android/axisandalliescompanion/BuyLIstAdapter.java
+++ app/src/main/java/com/example/android/axisandalliescompanion/BuyLIstAdapter.java
@@ -2,18 +2,19 @@ package com.example.android.axisandalliescompanion;
 
 
 import android.app.Activity;
-import android.content.Context;
-import android.support.annotation.LayoutRes;
-import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class BuyLIstAdapter extends ArrayAdapter<BuyList> {
+
+    protected Integer GtotalQuantityAsInt;
 
 
     public BuyLIstAdapter(Activity context, ArrayList<BuyList> BuyList, int unused) {
@@ -28,15 +29,50 @@ public class BuyLIstAdapter extends ArrayAdapter<BuyList> {
             listItemView = LayoutInflater.from(getContext()).inflate(R.layout.buy_list, parent, false);
         }
 
-        BuyList currentItem = getItem(position);
+        final BuyList currentItem = getItem(position);
 
+
+        Button addOne = (Button) listItemView.findViewById(R.id.addOneButton);
+        Button addFive= (Button) listItemView.findViewById(R.id.addFiveButton);
+        Button subOne = (Button) listItemView.findViewById(R.id.subtractOneButton);
+        Button clearItem = (Button) listItemView.findViewById(R.id.singleItemClearButton);
+
+        TextView totalQuantity = (TextView) listItemView.findViewById(R.id.currentPurchedCount);
+
+
+        addOne.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+           }
+       });
+        addFive.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        subOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        clearItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
+
+
+
+
+
+        //sets the image
         ImageView pieceView = (ImageView) listItemView.findViewById(R.id.pieceImage);
         pieceView.setImageResource(currentItem.getImage());
         return listItemView;
     }
-
-
-
-
-
 }
